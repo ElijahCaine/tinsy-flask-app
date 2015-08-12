6a13af9ade1fd624b37e785baee0a218b75ba8cf
ElijahCaine <voigte@osuosl.org>
2015-08-12 14:53:15 -0700

    Modified content to help gist not have a sizeure

--- script.py
+++ script.py
@@ -5,12 +5,12 @@ app = Flask(__name__)
 @app.route('/')
 def hello():
     resp = ("<h1>You did it!</h1>"+
-           "<img src='/img.gif'/>")
+           "<img src='/img'/>")
     return resp
 
-@app.route('/img.gif')
+@app.route('/img')
 def img():
-    return send_file('+1.gif', mimetype='image/gif')
+    return send_file('z.gif', mimetype='image/gif')
 
 if __name__ == "__main__":
     app.run()
