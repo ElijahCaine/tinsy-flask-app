5a12424cec3f0dd3f77303dd1f010d4094e13071
ElijahCaine <voigte@osuosl.org>
2015-08-14 13:16:58 -0700

    Committed working script.

--- script.py
+++ script.py
@@ -1,11 +1,15 @@
 #!/usr/bin/env python
 from flask import Flask, send_file
+from datetime import datetime
+
 app = Flask(__name__)
 
 @app.route('/')
 def hello():
+    now = datetime.now()
     resp = ("<h1>You did it!</h1>"+
-           "<img src='/img'/>")
+           "<img src='/img'/>"+
+           "<p>Today's date is: "+now.strftime("%Y-%m-%d, %H:%M:%S"))
     return resp
 
 @app.route('/img')
@@ -13,4 +17,4 @@ def img():
     return send_file('z.gif', mimetype='image/gif')
 
 if __name__ == "__main__":
-    app.run()
+    app.run(host='0.0.0.0')
